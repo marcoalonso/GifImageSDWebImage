b2c6b7cb8568918c09be637ac8d01ad7bb890769
Marco Alonso Rodriguez <marcoalonsorota@hotmail.com>
2024-12-12 13:34:55 -0600

    Initial Commit

---
+++ GifImageDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GifImageDemo
+//
+//  Created by Marco Alonso Rodriguez on 12/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
